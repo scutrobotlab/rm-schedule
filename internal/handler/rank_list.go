a9656714fafb2f957b5133978b71a82404062380
Summer-lights <1067088037@qq.com>
2024-05-27 17:04:41 +0800

    feat: add CompleteForm

--- internal/handler/rank_list.go
+++ internal/handler/rank_list.go
@@ -10,11 +10,25 @@ import (
 	"log"
 )
 
+type RankListItem struct {
+	RankScoreItem RankScoreItem `json:"rankScoreItem"`
+	CompleteForm  CompleteForm  `json:"completeForm"`
+}
+
 type RankScoreItem struct {
-	Rank          int     `json:"Rank"`
-	SchoolChinese string  `json:"SchoolChinese"`
-	SchoolEnglish string  `json:"SchoolEnglish"`
-	Score         float64 `json:"Score"`
+	Rank          int     `json:"rank"`
+	SchoolChinese string  `json:"schoolChinese"`
+	SchoolEnglish string  `json:"schoolEnglish"`
+	Score         float64 `json:"score"`
+}
+
+type CompleteForm struct {
+	School                string `json:"school"`
+	Team                  string `json:"team"`
+	Score                 int    `json:"score"`
+	InitialCoinDocument   int    `json:"initialCoinDocument"`
+	InitialCoinTechnology int    `json:"initialCoinTechnology"`
+	InitialCoinTotal      int    `json:"initialCoinTotal"`
 }
 
 func RankListHandler(c *gin.Context) {
@@ -24,33 +38,49 @@ func RankListHandler(c *gin.Context) {
 		return
 	}
 
-	_rankScoreMap, b := svc.Cache.Get("rank_score")
-	if b {
-		rankScoreMap := _rankScoreMap.(map[string]RankScoreItem)
-		if _, ok := rankScoreMap[schoolName]; !ok {
-			c.JSON(404, gin.H{"code": -1, "msg": "School not found"})
+	completedFormMap, ok := svc.Cache.Get("completed_form")
+	if !ok {
+		completedFormJson := make([]CompleteForm, 0)
+		err := json.Unmarshal(static.CompleteFormBytes, &completedFormJson)
+		if err != nil {
+			log.Printf("Failed to parse completed form: %v\n", err)
+			c.JSON(500, gin.H{"code": -1, "msg": "Failed to parse completed form"})
 			return
 		}
-		c.Header("Cache-Control", "public, max-age=3600")
-		c.JSON(200, rankScoreMap[schoolName])
+
+		completedFormMap = lo.SliceToMap(completedFormJson, func(item CompleteForm) (string, CompleteForm) { return item.School, item })
+		svc.Cache.Set("completed_form", completedFormMap, cache.NoExpiration)
+	}
+
+	completedForm, ok := completedFormMap.(map[string]CompleteForm)[schoolName]
+	if !ok {
+		c.JSON(404, gin.H{"code": -1, "msg": "School not found"})
 		return
 	}
 
-	rankScoreJson := make([]RankScoreItem, 0)
-	err := json.Unmarshal(static.RankScoreBytes, &rankScoreJson)
-	if err != nil {
-		log.Printf("Failed to parse rank list: %v\n", err)
-		c.JSON(500, gin.H{"code": -1, "msg": "Failed to parse rank list"})
-		return
+	rankScoreMap, ok := svc.Cache.Get("rank_score")
+	if !ok {
+		rankScoreJson := make([]RankScoreItem, 0)
+		err := json.Unmarshal(static.RankScoreBytes, &rankScoreJson)
+		if err != nil {
+			log.Printf("Failed to parse rank list: %v\n", err)
+			c.JSON(500, gin.H{"code": -1, "msg": "Failed to parse rank list"})
+			return
+		}
+
+		rankScoreMap = lo.SliceToMap(rankScoreJson, func(item RankScoreItem) (string, RankScoreItem) { return item.SchoolChinese, item })
+		svc.Cache.Set("rank_list", rankScoreMap, cache.NoExpiration)
 	}
 
-	rankScoreMap := lo.SliceToMap(rankScoreJson, func(item RankScoreItem) (string, RankScoreItem) { return item.SchoolChinese, item })
-	svc.Cache.Set("rank_list", rankScoreMap, cache.NoExpiration)
-	if _, ok := rankScoreMap[schoolName]; !ok {
+	rankScore, ok := rankScoreMap.(map[string]RankScoreItem)[schoolName]
+	if !ok {
 		c.JSON(404, gin.H{"code": -1, "msg": "School not found"})
 		return
 	}
 
 	c.Header("Cache-Control", "public, max-age=3600")
-	c.JSON(200, rankScoreMap[schoolName])
+	c.JSON(200, RankListItem{
+		RankScoreItem: rankScore,
+		CompleteForm:  completedForm,
+	})
 }
--- internal/static/load_embed.go
+++ internal/static/load_embed.go
@@ -2,6 +2,9 @@ package static
 
 import _ "embed"
 
+//go:embed complete_form.json
+var CompleteFormBytes []byte
+
 //go:embed rank_score.json
 var RankScoreBytes []byte
 
